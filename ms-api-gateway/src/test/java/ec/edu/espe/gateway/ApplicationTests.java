371bc5395ff9573af9a86ce8faf2af62b9c635db
SantiagoDavid18 <v04santillan@gmail.com>
2025-08-07 23:26:39 -0500

    Add files via upload

---
+++ ms-api-gateway/src/test/java/ec/edu/espe/gateway/ApplicationTests.java
@@ -0,0 +1,15 @@
+package ec.edu.espe.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
+
